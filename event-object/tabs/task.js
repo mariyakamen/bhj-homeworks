f5518fa5a8e2e6e824a9a7771ca3e5c5c5188deb
mariyak <mari.mts144@gmail.com>
2021-04-06 23:54:57 +0200

    correction to "tabs"

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -5,7 +5,21 @@ function click(event) {
   }
 
   event.target.classList.add("tab_active");
-  event.target.classList.add("tab__content_active");
+  let index = Array.from(event.target.parentElement.children).indexOf(
+    event.target
+  );
+
+  for (const each of document.getElementsByClassName("tab__content")) {
+    if (each === event.target.parentElement.querySelectorAll(".tab__content"))
+      continue;
+    each.classList.remove("tab__content_active");
+  }
+
+  let htmlObj = document
+    .getElementsByClassName("tab__contents")[0]
+    .querySelectorAll(".tab__content")[index];
+
+  htmlObj.classList.add("tab__content_active");
 
   return false;
 }
